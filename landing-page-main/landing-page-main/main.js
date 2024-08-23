a8b13e09c4caef3ba9218fbd1c839abd7fd012dc
omdubey05 <133864424+omdubey05@users.noreply.github.com>
2024-08-23 20:44:38 +0530

    Add files via upload
    Implemented dynamic content display based on user selections, enhancing user experience.
    Conducted A/B testing to optimize page layout and increase conversion rates.

---
+++ landing-page-main/landing-page-main/main.js
@@ -0,0 +1,6 @@
+function budChangetoblack(id){
+    document.getElementById(id).src = "./Assets/studiobuds-black.png"
+}
+function budChangetored(id){
+    document.getElementById(id).src = "./Assets/studiobuds-red.png"
+}
